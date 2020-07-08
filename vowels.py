84eb29d92be7605b200ea139c0a28488ebe2d3a6
Kushal Neupane <mkushal10@gmail.com>
2020-07-08 11:53:12 +0545

    Find Vowel in a sentence

---
+++ vowels.py
@@ -0,0 +1,18 @@
+#asking input
+input_num = input("Please Enter Sentence : ")
+#converting string into lower case
+string_num = input_num.lower()
+
+print("You typed : ",string_num)
+
+#initializing variable i as 0 that contains number of vowels
+i = 0
+#list
+list = ["a","e","i","o","u"]
+
+#this loop serach each character in a list
+for char in string_num:
+    if char in list: #checks the character is in list, if character in the list then it will increment the i by one
+        i=i+1
+
+print("There are ",i,"vowels in the sentence.")
