bc12eb8fc9c896910ea9111a5198c10407b8cc10
Kushal Neupane <39453694+mkushal10@users.noreply.github.com>
2020-07-08 11:55:02 +0545

    Update and rename vowels.py to find_vowels.py

--- find_vowels.py
+++ find_vowels.py
@@ -3,6 +3,7 @@ input_num = input("Please Enter Sentence : ")
 #converting string into lower case
 string_num = input_num.lower()
 
+#printing sentence
 print("You typed : ",string_num)
 
 #initializing variable i as 0 that contains number of vowels
@@ -15,4 +16,5 @@ for char in string_num:
     if char in list: #checks the character is in list, if character in the list then it will increment the i by one
         i=i+1
 
+#printing total
 print("There are ",i,"vowels in the sentence.")
